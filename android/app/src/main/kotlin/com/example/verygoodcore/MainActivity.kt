e300a416617aae5b94988ca2b607116a6b049b04
Gene <geneyllanes@gmail.com>
2022-02-26 14:01:49 -0500

    initialized with `very_good create`

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.kemal_tarriffs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2021, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import 'package:kemal_tarriffs/app/app.dart';
+import 'package:kemal_tarriffs/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
